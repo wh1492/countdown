2940090bdeec5e4a340c5acf1aba9f02865f2635
Leonelo Acaban <leoneloacaban@gmail.com>
2022-06-28 19:44:51 -0300

    fix: solventado con useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  let timepass = 0;
-  let tiempin;
   const [seconds, setSeconds] = useState(0);
   const [started, setStarted] = useState(false);
   const [time, setTime] = useState(0);
@@ -18,46 +16,30 @@ function App() {
   }
 
   function pauseCountdown() {
-    if (started) {
-      console.log("corriendo");
-    } else {
-      console.log("not corriendo");
-    }
     setStarted(false);
     clearInterval(time);
-    clearInterval(tiempin);
   }
 
   function resetCountdown() {
     setSeconds(0);
+  //  clearInterval(time);
   }
 
-  // function decrease() {
-  //   timepass = timepass + 1;
-  //   console.log("paso el contador decrease", timepass);
-  //   setSeconds((prevSeconds) => prevSeconds - 1);
-  //   // setSeconds(seconds - 1);
-  //   // Number(seconds)
-  //   if (seconds === timepass) {
-  //     console.log("finalizar");
-  //     clearInterval(time);
-  //   }
-  // }
+  useEffect(() => {
+    console.log("cambio seconds:", seconds);
+    if (seconds <= 0) {
+      console.log("finalizar");
+      setStarted(false);
+      clearInterval(time);
+    }
+  }, [seconds]);
 
   function countDown() {
-    // setTime(setInterval(decrease, 1000));
-    const tiempin = setInterval(() => {
-      timepass = timepass + 1;
-      console.log("paso el contador decrease", timepass);
-      setSeconds((prevSeconds) => prevSeconds - 1);
-      if (seconds === timepass) {
-        console.log("finalizar");
-        clearInterval(tiempin);
-      }
-    }, 1000);
-    
-
-    return tiempin;
+    setTime(
+      setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000)
+    );
   }
 
   return (
